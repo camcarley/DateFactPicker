12803425147966ad3d6cd17bfd38de5c07e16dc2
Cameron Carley <cam.carley@gmail.com>
2022-12-14 19:32:32 -0500

    update date util spec to make it more DRY

--- src/utils/specs/date.util.spec.ts
+++ src/utils/specs/date.util.spec.ts
@@ -1,38 +1,25 @@
 import { dateToString, getMonthName } from "../date.util";
-import { describe, it, expect } from "vitest";
-import { MonthNumber } from "../../types/month";
+import { describe, it, expect, test } from "vitest";
+import { MockMonths as months } from "../mocks/month.mock";
 
-let months: Array<{ month: MonthNumber; name: string }> = [
-  { month: 1, name: "January" },
-  { month: 2, name: "February" },
-  { month: 3, name: "March" },
-  { month: 4, name: "April" },
-  { month: 5, name: "May" },
-  { month: 6, name: "June" },
-  { month: 7, name: "July" },
-  { month: 8, name: "August" },
-  { month: 9, name: "September" },
-  { month: 10, name: "October" },
-  { month: 11, name: "November" },
-  { month: 12, name: "December" },
-];
+describe("Date Util", () => {
+  test("dateToString", () => {
+    months.forEach(({ month, name }) => {
+      it("should return a string of the correct month and day", () => {
+        expect(dateToString(1, month)).toBe(`${name} 1`);
+      });
 
-describe("dateToString", () => {
-  months.forEach(({ month, name }) => {
-    it("should return a string of the correct month and day", () => {
-      expect(dateToString(1, month)).toBe(`${name} 1`);
-    });
-
-    it("should return a string of the correct month and day", () => {
-      expect(dateToString(31, month)).not.toBe(`${name} 1`);
+      it("should return a string of the correct month and day", () => {
+        expect(dateToString(31, month)).not.toBe(`${name} 1`);
+      });
     });
   });
-});
 
-describe("getMonthName", () => {
-  months.forEach(({ month, name }) => {
-    it(`should return the month name for ${month}`, () => {
-      expect(getMonthName(month)).toBe(name);
+  test("getMonthName", () => {
+    months.forEach(({ month, name }) => {
+      it(`should return the month name for ${month}`, () => {
+        expect(getMonthName(month)).toBe(name);
+      });
     });
   });
 });
